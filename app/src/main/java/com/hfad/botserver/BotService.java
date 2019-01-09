0992b12d54239a37b8217c036695ef115d374d8d
electro1 <electro1@electro1.hook>
2019-01-09 16:40:20 -0300

    agrege la persistencia del servicio

---
+++ app/src/main/java/com/hfad/botserver/BotService.java
@@ -0,0 +1,191 @@
+package com.hfad.botserver;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
+public class BotService extends Service {
+
+    // Binder given to clients
+    private final IBinder mBinder = new LocalBinder();
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    private int numberTest = 0;
+    //MyTask myTask = null;
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        BotService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return BotService.this;
+        }
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if(numberTest == 0) {
+            numberTest++;
+            Thread check = new Thread(new CheckAlive());
+            check.start();
+            Toast.makeText(this, "Servicio creado!", Toast.LENGTH_SHORT).show();
+        }
+
+
+        /*if(myTask == null) {
+            Toast.makeText(this, "Servicio creado!", Toast.LENGTH_SHORT).show();
+            myTask = new MyTask();
+            myTask.execute();
+        }*/
+
+        return START_STICKY;//super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Servicio destruído!", Toast.LENGTH_SHORT).show();
+        //myTask.cancel(true);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    private class CheckAlive extends Thread {
+        @Override
+        public void run() {
+
+            Server.getInstance();//.initDataBase(this);
+            XMPPCliente.getInstance().runLoggin("lamulita","Portal06","404.city");
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(BotService.this,"1234")
+                            .setSmallIcon(android.R.drawable.stat_notify_sync)
+                            .setContentTitle("Mensaje de Alerta")
+                            .setContentText("Ejemplo de notificación.");
+                            //.setOngoing(true);
+
+
+
+            //Intent notIntent = intent;
+
+            //PendingIntent contIntent =
+            //        PendingIntent.getActivity(
+            //                this, 0, notIntent, 0);
+
+            //mBuilder.setContentIntent(contIntent);
+
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+            Notification notification = mBuilder.build();
+            mNotificationManager.notify(1234, notification);
+
+            startForeground(1234,notification);
+
+            /*while(true) {
+                try {
+                    //Server.getInstance();
+                    //XMPPCliente.getInstance();
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }*/
+        }
+    }
+
+    /*private class MyTask extends AsyncTask<String, String, String> {
+
+        private DateFormat dateFormat;
+        private String date;
+        private boolean cent;
+        private int miNumber = 0;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dateFormat = new SimpleDateFormat("HH:mm:ss");
+            cent = true;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            while (cent){
+                date = dateFormat.format(new Date());
+                try {
+                    publishProgress(date);
+                    // Stop 5s
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+        //    miNumber++;
+            Toast.makeText(getApplicationContext(), "Hora actual: " + values[0] + " Number: " + miNumber, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            cent = false;
+        }
+
+        public int increseNumber()
+        {
+            return ++miNumber;
+        }
+
+    }*/
+
+    public int getNumberTest()
+    {
+        return 0;//myTask.increseNumber();
+    }
+
+    public Server getServerInstance(){
+        return Server.getInstance();
+    }
+    public XMPPCliente getXMPPInstance()
+    {
+        return XMPPCliente.getInstance();
+    }
+}
--- app/src/main/java/com/hfad/botserver/MainActivity.java
+++ app/src/main/java/com/hfad/botserver/MainActivity.java
@@ -1,24 +1,41 @@
 package com.hfad.botserver;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
+
 public class MainActivity extends AppCompatActivity {
+
+    BotService mService = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Server.getInstance().initDataBase(this);
-        XMPPCliente.getInstance().runLoggin("lamulita","Portal06","404.city");
-        String ip = Server.getInstance().getIpAddress();
-        TextView label = (TextView) findViewById(R.id.ipServer);
-        label.setText(ip);
+
+
+        //String ip = Server.getInstance().getIpAddress();
+        //TextView label = (TextView) findViewById(R.id.ipServer);
+        //label.setText(ip);
 
         Button verNodos = (Button) findViewById(R.id.btNodes);
 
@@ -27,10 +44,62 @@ public class MainActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 Intent intent = new Intent(MainActivity.this, ListaNodeActivity.class);
                 startActivity(intent);
+                //stopService(new Intent(MainActivity.this, BotService.class));
+
             }
         });
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(mService == null) {
+
+            startService(new Intent(MainActivity.this, BotService.class));
+           // startForegroundService(new Intent(MainActivity.this, BotService.class));
+            Intent intent = new Intent(this, BotService.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+
+    }
+
+    public void onBtTestClick(View v) {
+        mService.getNumberTest();
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            BotService.LocalBinder binder = (BotService.LocalBinder) service;
+            mService = binder.getService();
+            mService.getServerInstance().initDataBase(MainActivity.this);
+
+            String ip = mService.getServerInstance().getIpAddress();
+            TextView label = (TextView) findViewById(R.id.ipServer);
+            label.setText(ip);
+
+//            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+//            mBound = false;
+        }
+
+        @Override
+        public void onNullBinding(ComponentName name)
+        {
+            Log.i("Service","NULL");
+        }
+
+    };
 
 
 }
